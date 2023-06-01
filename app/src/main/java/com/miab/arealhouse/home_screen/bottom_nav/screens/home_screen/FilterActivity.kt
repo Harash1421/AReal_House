b3ce79f81ab6d3a0a0d05f1bd4e69f683ed32395
Harash_1421 <abdulaziz.harash@hotmail.com>
2023-06-01 15:35:12 +0300

    Complete Features on Filter Screen

--- app/src/main/java/com/miab/arealhouse/home_screen/bottom_nav/screens/home_screen/FilterActivity.kt
+++ app/src/main/java/com/miab/arealhouse/home_screen/bottom_nav/screens/home_screen/FilterActivity.kt
@@ -1,13 +1,18 @@
 package com.miab.arealhouse.home_screen.bottom_nav.screens.home_screen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Clear
@@ -19,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.miab.arealhouse.R
@@ -54,11 +60,29 @@ class FilterActivity : ComponentActivity() {
                         val bathroomCount = remember { mutableIntStateOf(0) }
                         val parkingCount = remember { mutableIntStateOf(0) }
 
+                        val facilities = remember {
+                            mutableStateOf(
+                                mapOf(
+                                    "WiFi" to false,
+                                    "AC" to false,
+                                    "Fully Furnished" to false,
+                                    "24 Hour Access" to false
+                                )
+                            )
+                        }
+
                         val accommodations = stringArrayResource(id = R.array.accommodation_types)
                         val conditions = stringArrayResource(id = R.array.property_conditions)
-
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            AccommodationTypePicker(accommodations, selectedAccommodation) { selectedAccommodation = it }
+                        Box(modifier = Modifier.fillMaxSize()) {
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            AccommodationTypePicker(
+                                accommodations,
+                                selectedAccommodation
+                            ) { selectedAccommodation = it }
 
                             Divider(modifier = Modifier.padding(vertical = 16.dp))
 
@@ -73,11 +97,47 @@ class FilterActivity : ComponentActivity() {
 
                             Divider(modifier = Modifier.padding(vertical = 16.dp))
 
-                            PropertyConditionPicker(conditions, selectedCondition) { selectedCondition = it }
+                            PropertyConditionPicker(
+                                conditions,
+                                selectedCondition
+                            ) { selectedCondition = it }
 
                             Divider(modifier = Modifier.padding(vertical = 16.dp))
 
-                            FacilitySection(bedroomCount, bathroomCount, parkingCount)
+                            FacilitySection(
+                                bedroomCount,
+                                bathroomCount,
+                                parkingCount,
+                                facilities
+                            ) { facility ->
+                                facilities.value = facilities.value.toMutableMap().apply {
+                                    this[facility] = !(this[facility] ?: false)
+                                }
+                            }
+
+                            Spacer(modifier = Modifier.height(48.dp))
+                        }
+
+                            Box(modifier = Modifier.align(Alignment.BottomStart)) {
+                                FilterActions(
+                                    onReset = {
+                                        // Reset your filters here
+                                        selectedAccommodation = 0
+                                        selectedCondition = 0
+                                        price = 0f
+                                        minPrice = ""
+                                        maxPrice = ""
+                                        bedroomCount.value = 0
+                                        bathroomCount.value = 0
+                                        parkingCount.value = 0
+                                        facilities.value = facilities.value.mapValues { false }
+                                    },
+                                    onApply = {
+                                        // Apply your filters here
+                                        // Your code here
+                                    }
+                                )
+                            }
                         }
                     }
                 }
@@ -85,6 +145,7 @@ class FilterActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Composable
 fun AccommodationTypePicker(items: Array<String>, selectedItem: Int, onItemSelected: (Int) -> Unit) {
@@ -158,8 +219,8 @@ fun PriceSection(
             onMaxPriceChange(formatter.format(range.endInclusive))
         },
         colors = SliderDefaults.colors(
-            thumbColor = Color.Red,
-            activeTrackColor = Color.Red,
+            thumbColor = Color.Green,
+            activeTrackColor = Color.Blue,
             inactiveTrackColor = Color.LightGray
         ),
         steps = 100,
@@ -199,14 +260,55 @@ fun PriceSection(
 
 @Composable
 fun PropertyConditionPicker(items: Array<String>, selectedItem: Int, onItemSelected: (Int) -> Unit) {
-    // Similar to AccommodationTypePicker
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val icon = if (expanded) {
+        Icons.Filled.KeyboardArrowUp
+    } else {
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    Text(text = "Property Type", style = MaterialTheme.typography.h6)
+    Spacer(modifier = Modifier.height(8.dp))
+    Box {
+        OutlinedTextField(
+            value = items[selectedItem],
+            onValueChange = {},
+            readOnly = true,
+            modifier = Modifier.fillMaxWidth(),
+            trailingIcon = {
+                Icon(
+                    icon,
+                    contentDescription = "",
+                    Modifier.clickable { expanded = !expanded }
+                )
+            }
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items.forEachIndexed { index, item ->
+                DropdownMenuItem(onClick = {
+                    onItemSelected(index)
+                    expanded = false
+                }) {
+                    Text(text = item)
+                }
+            }
+        }
+    }
 }
 
 @Composable
 fun FacilitySection(
     bedroomCount: MutableState<Int>,
     bathroomCount: MutableState<Int>,
-    parkingCount: MutableState<Int>
+    parkingCount: MutableState<Int>,
+    facilities: MutableState<Map<String, Boolean>>,
+    onFacilityToggle: (String) -> Unit
 ) {
     Text(text = "Facility", style = MaterialTheme.typography.h6)
     Spacer(modifier = Modifier.height(8.dp))
@@ -215,6 +317,21 @@ fun FacilitySection(
     QuantityPicker("Bathroom", bathroomCount.value) { newValue -> bathroomCount.value = newValue }
     Spacer(modifier = Modifier.height(8.dp))
     QuantityPicker("Parking", parkingCount.value) { newValue -> parkingCount.value = newValue }
+    Spacer(modifier = Modifier.height(8.dp))
+
+    Divider(modifier = Modifier.padding(vertical = 14.dp))
+
+    Text(text = "Boarding Facilities", style = MaterialTheme.typography.h6)
+    Spacer(modifier = Modifier.height(8.dp))
+
+    facilities.value.keys.forEach { facility ->
+        CheckboxOption(
+            text = facility,
+            checked = facilities.value[facility] ?: false,
+            onCheckedChange = { onFacilityToggle(facility) }
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+    }
 }
 
 @Composable
@@ -222,14 +339,70 @@ fun QuantityPicker(label: String, quantity: Int, onQuantityChange: (Int) -> Unit
     Row(verticalAlignment = Alignment.CenterVertically) {
         Text(text = label, style = MaterialTheme.typography.body1, modifier = Modifier.weight(1f))
         IconButton(onClick = { if (quantity > 0) onQuantityChange(quantity - 1) }) {
-            Icon(Icons.Filled.Clear, contentDescription = "Decrease")
+            Icon(Icons.Filled.AddCircle, contentDescription = "Decrease")
         }
         Text(text = quantity.toString(), style = MaterialTheme.typography.body1)
         IconButton(onClick = { onQuantityChange(quantity + 1) }) {
-            Icon(Icons.Filled.Add, contentDescription = "Increase")
+            Icon(Icons.Filled.AddCircle, contentDescription = "Increase")
         }
     }
 }
+
+
+@Composable
+fun CheckboxOption(text: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = text, style = MaterialTheme.typography.body1)
+    }
+}
+
+@Composable
+fun FilterActions(onReset: () -> Unit, onApply: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(78.dp)
+            .padding(12.dp)
+            .background(Color.White),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        TextButton(
+            onClick = { onReset() },
+            modifier = Modifier
+                .weight(0.4f)
+                .fillMaxHeight()
+                .padding(10.dp)
+        ) {
+            Text(text = "Reset",
+                modifier = Modifier.wrapContentSize(Alignment.CenterStart),
+                style = TextStyle(color = Color.Blue)
+            )
+        }
+
+        Button(
+            onClick = { onApply() },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
+            modifier = Modifier
+                .weight(0.7f)
+                .fillMaxHeight()
+                .padding(7.dp)
+        ) {
+            Text(text = "Apply",
+                modifier = Modifier.wrapContentSize(Alignment.Center),
+                style = TextStyle(color = Color.White)
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
